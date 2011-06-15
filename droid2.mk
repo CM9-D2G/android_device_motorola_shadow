653be9a9b04c13f25018dfceeeea8ac9acee8472
Jon Stanford <jbirdvegas@gmail.com>
2011-06-15 12:39:48 -0700

    http://rootzwiki.com/showthread.php?374-DEV_THREAD-CyanogenMod-7-port-for-Droid-2-based-of-cvpcs-cm4dx&p=4893&viewfull=1#post4893

--- droid2.mk
+++ droid2.mk
@@ -61,7 +61,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83 \
     ro.cdma.data_retry_config=default_randomization=2000,0,0,120000,180000,540000,960000 \
     ro.com.motorola.smartsensor=true \
-    ro.media.capture.maxres=8m \
+    ro.media.capture.maxres=5m \
     ro.media.capture.fast.fps=4 \
     ro.media.capture.slow.fps=60 \
     ro.media.capture.flash=led \
