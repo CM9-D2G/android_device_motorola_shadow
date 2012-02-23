8efdbd04d41744261f5c756d3a3e049a5aa48dd7
Dustin Einglett <x13thangelx@gmail.com>
2012-02-23 17:01:56 -0600

    Cherry-pick libcameradafy from github.com/tpruvot/android_device_motorola_jordan/tree/ics_mb525/libcameradefy

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,14 +1,16 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),shadow)
+ifeq ($(TARGET_DEVICE),droid2we)
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS    := optional
 LOCAL_MODULE_PATH    := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE         := camera.shadow
+LOCAL_MODULE         := camera.droid2we
 LOCAL_SRC_FILES      := cameraHal.cpp
 LOCAL_PRELINK_MODULE := false
+
+LOCAL_SRC_FILES += v4l2_utils.c overlay.cpp
 
 LOCAL_SHARED_LIBRARIES += \
     liblog \
@@ -19,10 +21,9 @@ LOCAL_SHARED_LIBRARIES += \
     libhardware \
     libcamera_client \
     libcamera \
-    libui
 
 LOCAL_SHARED_LIBRARIES += libdl
 
 include $(BUILD_SHARED_LIBRARY)
 
-endif #BOARD_QCOM_BASED_CAMERA_HAL
+endif #TARGET_DEVICE
