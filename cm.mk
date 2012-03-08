e930d79bbafe8a9e4952a7887da925b5df948a8c
Dustin Einglett <x13thangelx@gmail.com>
2012-03-08 15:25:39 -0600

    Convert to common repo format

--- cm.mk
+++ cm.mk
@@ -1,25 +1,19 @@
-## Specify phone tech before including full_phone
-#$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/motorola/shadow/shadow.mk)
 
-# Inherit our vendor filez, yo.
-$(call inherit-product, vendor/motorola/shadow/shadow-vendor.mk)
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_shadow
 PRODUCT_BRAND := verizon
 PRODUCT_DEVICE := shadow
-PRODUCT_MODEL := Droid X
+PRODUCT_MODEL := DROID X
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_SFX := vzw
 
 # Release name and versioning
-PRODUCT_RELEASE_NAME := DroidX
+PRODUCT_RELEASE_NAME := DROIDX
 
 UTC_DATE := $(shell date +%s)
 DATE := $(shell date +%Y%m%d)
