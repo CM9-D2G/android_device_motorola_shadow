7ccf6cb0f11f3d77f2191147c996e69a66bd7447
Dustin Einglett <x13thangelx@gmail.com>
2012-03-08 16:24:20 -0600

    More converting to common repo

--- cm.mk
+++ cm.mk
@@ -5,15 +5,9 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, device/motorola/shadow/shadow.mk)
 
 ## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := shadow
 PRODUCT_NAME := cm_shadow
 PRODUCT_BRAND := verizon
-PRODUCT_DEVICE := shadow
 PRODUCT_MODEL := DROID X
 PRODUCT_MANUFACTURER := Motorola
-PRODUCT_SFX := vzw
 
-# Release name and versioning
-PRODUCT_RELEASE_NAME := DROIDX
-
-UTC_DATE := $(shell date +%s)
-DATE := $(shell date +%Y%m%d)
--- device.mk
+++ device.mk
@@ -13,16 +13,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This file includes all definitions that apply only to shadow devices
+# This file includes all definitions that apply only to maguro devices
 #
-# Anything that is generic to all OMAP34xx/SGX530 products should go in the omap34com directory
+# Anything that is generic to all tuna products should go in the tuna directory
 #
 # Everything in this directory will become public
 
-#DEVICE_PACKAGE_OVERLAYS := device/motorola/shadow/overlay
+DEVICE_PACKAGE_OVERLAYS := device/motorola/shadow/overlay
 
 # These are the hardware-specific features
-PRODUCT_COPY_FILES += \
+PRODUCT_COPY_FILES := \
 	frameworks/base/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml 
 
 PRODUCT_PACKAGES := \
