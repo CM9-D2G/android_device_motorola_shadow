a0f93a62a2b0149db0e6fccdf5a62b0fecda958d
Jonathan Bither <jonbither@gmail.com>
2012-03-24 16:19:19 -0400

    Updates

--- shadow.mk
+++ shadow.mk
@@ -24,8 +24,8 @@
 # Device overlay and prebuilt directories
 DEVICE_PREBUILT := device/motorola/shadow/prebuilt
 
-# Get the long list of APNs
-PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+# APNs
+PRODUCT_COPY_FILES := $(DEVICE_PREBUILT)/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 # Camera
 PRODUCT_PACKAGES := \
