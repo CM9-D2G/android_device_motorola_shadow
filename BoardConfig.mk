86c442bf611e28fbf2fb85d17dba70f7f5702e64
Dustin Einglett <x13thangelx@gmail.com>
2012-04-03 16:25:45 -0500

    I dont know wtf happened here.... Revert "Merge branch 'cm9' of github.com:CM9-D2G/android_device_motorola_shadow into cm9"
    This reverts commit 3cc7a63a5457dc52b7992bb8a2aadd39d9911f11, reversing
    changes made to 18b79f8b466382552f5952c35c85763ee4aa85f9.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,46 +1,31 @@
-# Droid X Board Config
+# Copyright (C) 2011 The Android Open Source Project
+# Copyright (C) 2012 bikedude880
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# Camera
-USE_CAMERA_STUB := false
-BOARD_USE_LEGACY_TOUCHSCREEN := true
-
-# inherit from the proprietary version
+# Use the non-open-source part, if present
 -include vendor/motorola/shadow/BoardConfigVendor.mk
 
+# Use the part that is common between all tunas
+include device/motorola/omap34com/BoardConfig.mk
+
 # Processor
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := omap3
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
 TARGET_BOOTLOADER_BOARD_NAME := shadow
-TARGET_CPU_SMP := false
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_ARCH_VARIANT_CPU := cortex-a8
-TARGET_ARCH_VARIANT_FPU := neon
-TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8
-TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8
 
 # Command line
 TARGET_PREBUILT_KERNEL := device/motorola/shadow/kernel
 BOARD_KERNEL_CMDLINE := console=ttyS2,115200n8 rw mem=498M@0x80C00000 init=/init ip=off brdrev=P3A androidboot.bootloader=0x0000 mmcparts=mmcblk1:p7(pds),p15(boot),p16(recovery),p17(cdrom),p18(misc),p19(cid),p20(kpanic),p21(system),p22(cache),p23(preinstall),p24(userdata) androidboot.mode=reboot androidboot.bootloader=D011 androidboot.serialno=0A3A94CF0602D02
-BOARD_KERNEL_BASE := 0x10000000
 
-# Sensors
-ENABLE_SENSORS_COMPAT := true
-
-# Do da wifi
-BOARD_WPA_SUPPLICANT_DRIVER := CUSTOM
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := libCustomWifi
-WPA_SUPPLICANT_VERSION      := VER_0_6_X
-BOARD_WLAN_DEVICE           := wl1271
-#BOARD_SOFTAP_DEVICE         := wl1271
-WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/tiwlan_drv.ko"
-BOARD_WLAN_TI_STA_DK_ROOT   := system/wlan/ti/wilink_6_1
-WIFI_DRIVER_MODULE_ARG      := ""
-WIFI_DRIVER_MODULE_NAME     := "tiwlan_drv"
-WIFI_FIRMWARE_LOADER        := "wlan_loader"
-WIFI_DRIVER_FW_STA_PATH     := "/system/etc/wifi/fw_wlan1271.bin"
-WIFI_DRIVER_FW_AP_PATH      := "/system/etc/wifi/fw_tiwlan_ap.bin"
 # Audio
 BOARD_USES_GENERIC_AUDIO := false
 BOARD_USES_AUDIO_LEGACY := true
@@ -53,84 +38,14 @@ TARGET_PROVIDES_LIBAUDIO := true
 BOARD_OVERLAY_BASED_CAMERA_HAL := true
 
 # Recovery
-BOARD_HAS_LOCKED_BOOTLOADER := true
 TARGET_PREBUILT_RECOVERY_KERNEL := device/motorola/shadow/kernel
-BOARD_ALWAYS_INSECURE := true
-BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_MKE2FS := device/motorola/shadow/releaseutils/mke2fs
-TARGET_RECOVERY_PRE_COMMAND := "echo 1 > /data/.recovery_mode; sync;"
-TARGET_RECOVERY_PRE_COMMAND_CLEAR_REASON := true
-
-#BOARD_BOOTIMAGE_PARTITION_SIZE := 0x003fffff
-#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x004fffff
-#BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x0bdfffff   # limited so we enforce room to grow
-#BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
-
-# Graphics
-BOARD_EGL_CFG := device/motorola/shadow/prebuilt/etc/egl.cfg
-COMMON_GLOBAL_CFLAGS += -DMISSING_EGL_EXTERNAL_IMAGE -DMISSING_EGL_PIXEL_FORMAT_YV12 -DMISSING_GRALLOC_BUFFERS
-COMMON_GLOBAL_CFLAGS += -DTARGET_OMAP3 -DOMAP_COMPAT -DMOTOROLA_UIDS
-BOARD_NO_RGBX_8888 := true
-DEFAULT_FB_NUM := 0
-BOARD_USE_YUV422I_DEFAULT_COLORFORMAT := true
-
-# OMX
-HARDWARE_OMX := true
-ifdef HARDWARE_OMX
-OMX_VENDOR := ti
-OMX_VENDOR_WRAPPER := TI_OMX_Wrapper
-BOARD_OPENCORE_LIBRARIES := libOMX_Core
-BOARD_OPENCORE_FLAGS := -DHARDWARE_OMX=1
-BUILD_WITH_TI_AUDIO := 1
-TARGET_USE_OMX_RECOVERY := true
-endif
-OMX_TI_OMAP_TIER_LEVEL := 10
-
-# OMAP
-OMAP_ENHANCEMENT := true
-ifdef OMAP_ENHANCEMENT
-COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT -DTARGET_OMAP3
-endif
-
-# MOTOROLA
-USE_MOTOROLA_CODE := true
-ifdef USE_MOTOROLA_CODE
-COMMON_GLOBAL_CFLAGS += -DUSE_MOTOROLA_CODE
-endif
-USE_MOTOROLA_USERS := true
-ifdef USE_MOTOROLA_USERS
-COMMON_GLOBAL_CFLAGS += -DUSE_MOTOROLA_USERS
-endif
 
 TARGET_PROVIDES_INIT_RC := true
-
-# OTA Packaging
-TARGET_PROVIDES_RELEASETOOLS := true
-TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/motorola/shadow/releasetools/shadow_ota_from_target_files
-TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := device/motorola/shadow/releasetools/shadow_img_from_target_files
 
 # UMS
 BOARD_MASS_STORAGE_FILE_PATH := "/sys/devices/platform/usb_mass_storage/lun0/file"
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/usb_mass_storage/lun0/file"
 BOARD_USE_USB_MASS_STORAGE_SWITCH := true
 BOARD_CUSTOM_USB_CONTROLLER := ../../device/motorola/shadow/UsbController.cpp
-<<<<<<< HEAD
-
-# Hijack
-TARGET_NEEDS_MOTOROLA_HIJACK := true
-#BOARD_HIJACK_LOG_ENABLE := true
-
-# Misc.
-#BOARD_USE_BATTERY_CHARGE_COUNTER := true
-BOARD_FLASH_BLOCK_SIZE := 131072
-BOARD_NEEDS_CUTILS_LOG := true
-BOARD_USES_SECURE_SERVICES := true
-BOARD_ALWAYS_INSECURE := true
-BOARD_USES_KEYBOARD_HACK := true
-
-# TESTING
-TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
-DEBUG_BIONIC_LIBC := true
-=======
 BOARD_MTP_DEVICE := "/dev/mtp"
->>>>>>> 18b79f8b466382552f5952c35c85763ee4aa85f9
--- cm.mk
+++ cm.mk
@@ -1,6 +1,3 @@
-## Specify phone tech before including full_phone
-#$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
@@ -10,18 +7,10 @@ TARGET_BOOTANIMATION_NAME := vertical-480x854
 # Inherit device configuration
 $(call inherit-product, device/motorola/shadow/shadow.mk)
 
-# Inherit our vendor filez, yo.
-$(call inherit-product, vendor/motorola/shadow/shadow-vendor.mk)
-
 ## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := shadow
 PRODUCT_NAME := cm_shadow
 PRODUCT_BRAND := verizon
 PRODUCT_MODEL := DROIDX
 PRODUCT_MANUFACTURER := Motorola
-PRODUCT_SFX := vzw
 
-# Release name and versioning
-PRODUCT_RELEASE_NAME := DROIDX
-
-UTC_DATE := $(shell date +%s)
-DATE := $(shell date +%Y%m%d)
